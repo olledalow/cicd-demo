f4ca67fe57c71056367f7b23d5c37b5b63531498
Bence Ollé <olledalow@gmail.com>
2025-04-04 18:10:14 +0100

    Add failing endpoint test

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
+
+
+@app.get("/fail", status_code=404)
+def fail_endpoint():
+    return {"msg": "This should fail"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,8 @@ def test_root():
     response = client.get("/")  # type: ignore
     assert response.status_code == 200  # type: ignore
     assert response.json() == {"msg": "Hello World"}  # type: ignore
+
+
+def test_fail_endpoint():
+    response = client.get("/fail")
+    assert response.status_code == 200  # ❌ Wrong on purpose. endpoint returns 404
